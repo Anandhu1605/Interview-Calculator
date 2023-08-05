fb530978ffd8da079caba1b026b3780e0a48c73e
Anandhu1605 <118506014+Anandhu1605@users.noreply.github.com>
2023-08-05 11:30:38 +0530

    mycalci

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [value,setValue]=useState('')
+
+  const handleClick=(e)=>{
+    const expression = document.querySelector('input').value.replace('÷', '/');
+    setValue(value.concat(e.target?.value));
+    console.log(expression)
+ }
+
+  const calc=()=>{
+    try {
+      const expression = value.replace(/÷/g, '/').replace(/x/g, '*');
+      setValue(eval(expression).toString());
+    } catch (error) {
+      setValue("INVALID")
+   }
+  
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className='container'>
+    <div className='calculator'>
+      <form action=''>
+        <div className='display'>
+          <input type='text' value={value}/>
+        </div>
+        
+        <div>
+          
+          <input type='button' value="7"  onClick={handleClick}/>
+          <input type='button' value="8" onClick={handleClick}/>
+          <input type='button' value="9" onClick={handleClick}/>
+          <input type='button' value="÷" onClick={handleClick}/>
+        </div>
+        <div>
+        <input type='button' value="4" onClick={handleClick}/>
+        <input type='button' value="5" onClick={handleClick}/>
+        <input type='button' value="6" onClick={handleClick}/>
+        <input type='button' value="x" onClick={handleClick}/>
+        </div>
+        <div>
+        <input type='button' value="1" onClick={handleClick}/>
+        <input type='button' value="2" onClick={handleClick}/>
+        <input type='button' value="3" onClick={handleClick}/>
+        <input type='button' value="-" onClick={handleClick}/>
+        </div>
+
+        <div>
+        <input type='button' value="0" onClick={handleClick}/>
+        <input type='button' value="." onClick={handleClick} style={{fontWeight:"bolder",justifyContent:"space-between"}} />
+        <input type='button' value="+" onClick={handleClick}/>
+        <input type='button' value="=" onClick={calc} style={{backgroundColor:"orange", color:"white"}}/>
+        </div>
+
+
+
+  
+      </form>
     </div>
+   </div>
   );
 }
 
+
 export default App;
+
+
+
+
+
+
